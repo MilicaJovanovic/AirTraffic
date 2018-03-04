7350abec3f5763f5c2b0e63ce607eb3e78a09d81
Milica Jovanovic <j.milica95@yahoo.com>
2018-03-04 21:55:32 +0100

    Load airplane data periodicaly

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ const planeLeft = "<img style='width: 25px; height: 25px;' src='../images/plane-
 const planeRight = "<img style='width: 25px; height: 25px;' src='../images/plane-right.png' />";
 var lat = 0.0;
 var lng = 0.0;
+let lastPlaneListId = 0;
 
 $(document).ready(() => {
   $('#locationModal').modal('show');
@@ -15,7 +16,9 @@ $(document).ready(() => {
 geolocationAllowed = () => {
   $('#locationModal').modal('hide');
   getLocation();
-  loadAirplaneData();
+  setInterval(() => {
+    loadAirplaneData(lastPlaneListId);
+  }, 5000);
   document.getElementById("description").innerHTML = "Here you can see all the airplanes that are flying over you location. In the list above you can see fligth details available and sort airplanes by altitude they are flying at.";
 }
 
@@ -45,16 +48,20 @@ getLocation = () => {
 getLatLong = (position) => {
   lat = position.coords.latitude;
   lng = position.coords.longitude;
-  console.log("Latitude: " + typeof(position.coords.latitude) + 
-  "<br>Longitude: " + position.coords.longitude);
 }
 
 /* 
  * Loads airplane data from API by using fetch
  */
-loadAirplaneData = () => {
+loadAirplaneData = (lastDv) => {
+  let url = 'https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=' + lat + '&lng=' + lng + '&fDstL=0&fDstU=100';
+
+  if (lastDv != 0) {
+    url = url + '&ldv=' + lastDv;
+  }
+
   setTimeout(() => {
-    fetch('https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=' + lat + '&lng=' + lng + '&fDstL=0&fDstU=100')
+    fetch(url)
     .then((response) => {
       response.json()
       .then((data) => {
@@ -63,8 +70,10 @@ loadAirplaneData = () => {
 
         // Creating table content
         airplaneList.forEach((element) => {
-          const tableRow = generateTableRow(element);
-          table.appendChild(tableRow);
+          if (element.Alt !== undefined) {
+            const tableRow = generateTableRow(element);
+            table.appendChild(tableRow);
+          }
         });
       });
     })
@@ -126,7 +135,7 @@ back = () => {
   $.get('/templates/index.html', function(template, textStatus, jqXhr) {
     $('#route-view').html(Mustache.render($(template).filter('#index').html()))
   });
-  loadAirplaneData();
+  loadAirplaneData(lastPlaneListId);
 }
 
 /*
