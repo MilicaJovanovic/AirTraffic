dc54a83edc6be62f7ac98c36c13ebf901a30e5cd
Milica Jovanovic <j.milica95@yahoo.com>
2018-03-04 21:15:40 +0100

    Implementing Geolocation API and loading data by user location

--- js/index.js
+++ js/index.js
@@ -1,27 +1,79 @@
 // Defining constant IMG elements with airplane icons
 const planeLeft = "<img style='width: 25px; height: 25px;' src='../images/plane-left.png' />";
-const planeRight = "<img style='width: 25px; height: 25px;' src='../images/plane-right.png' />"
+const planeRight = "<img style='width: 25px; height: 25px;' src='../images/plane-right.png' />";
+var lat = 0.0;
+var lng = 0.0;
 
 $(document).ready(() => {
-  // Using fetch to get airplane data from API
-  fetch('https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=33.433638&lng=-112.008113&fDstL=0&fDstU=100')
-  .then((response) => {
-    response.json()
-    .then((data) => {
-      const airplaneList = data.acList;
-      const table = document.getElementById('data-table');
-
-      // Creating table content
-      airplaneList.forEach((element) => {
-        const tableRow = generateTableRow(element);
-        table.appendChild(tableRow);
-      });
-    });
-  })
-  .catch((err) => {
-    console.log('error ' + err);
-  });
+  $('#locationModal').modal('show');
 });
+
+/*
+ * If user clicks Yes, this function is called
+ * I hides modal, and load data
+ */
+geolocationAllowed = () => {
+  $('#locationModal').modal('hide');
+  getLocation();
+  loadAirplaneData();
+  document.getElementById("description").innerHTML = "Here you can see all the airplanes that are flying over you location. In the list above you can see fligth details available and sort airplanes by altitude they are flying at.";
+  document.getElementById("table-part").style.display = "block";
+}
+
+/*
+ * If user clicks No, this function is called
+ * It hides modal, and show message
+ */
+geolocationNotAllowed = () => {
+  $('#locationModal').modal('hide');
+  document.getElementById("description").innerHTML = "It is necessary to allow location to use the app!";
+}
+
+/*
+ * Retrieves user location with Geolocation API
+ */
+getLocation = () => {
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(getLatLong);
+  } else {
+      console.log("ne moze");
+  }
+}
+
+/*
+ * Gets user longitude and latitude
+ */
+getLatLong = (position) => {
+  lat = position.coords.latitude;
+  lng = position.coords.longitude;
+  console.log("Latitude: " + typeof(position.coords.latitude) + 
+  "<br>Longitude: " + position.coords.longitude);
+}
+
+/* 
+ * Loads airplane data from API by using fetch
+ */
+loadAirplaneData = () => {
+  setTimeout(() => {
+    fetch('https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=' + lat + '&lng=' + lng + '&fDstL=0&fDstU=100')
+    .then((response) => {
+      response.json()
+      .then((data) => {
+        const airplaneList = data.acList;
+        const table = document.getElementById('data-table');
+
+        // Creating table content
+        airplaneList.forEach((element) => {
+          const tableRow = generateTableRow(element);
+          table.appendChild(tableRow);
+        });
+      });
+    })
+    .catch((err) => {
+      console.log('error ' + err);
+    });
+  }, 1500);
+}
 
 /*
  * Creates HTML elements for airplane data
